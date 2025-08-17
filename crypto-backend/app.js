983dbaa05166a8077365056421e70ac3ca268745
Aldinokhalifah <khalifahaldino@gmail.com>
2025-08-17 16:38:16 +0700

    ubah konfig cors

--- crypto-backend/app.js
+++ crypto-backend/app.js
@@ -6,7 +6,12 @@ const routes = require('./routes/api');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://crypto-cek.vercel.app/", // atau ganti dengan URL frontend-mu di vercel
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
